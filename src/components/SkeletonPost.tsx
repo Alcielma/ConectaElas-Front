a11350232cb33bb05f47cadeb7a9580ee71aceb0
Rafael-acm1 <rafael-acm1@hotmail.com>
2025-04-22 17:55:41 -0300

    Style: Refatoração Skeleton do feed e correções simples de estilo em outros locais

--- src/components/SkeletonPost.tsx
+++ src/components/SkeletonPost.tsx
@@ -4,9 +4,15 @@ import "./SkeletonPost.css";
 const SkeletonPost: React.FC = () => {
   return (
     <div className="skeleton-post">
+      <div className="skeleton-header-post">
+        <div className="skeleton-profile-image"></div>
+        <div className="skeleton-profile-name-post"></div>
+      </div>
+      <div className="skeleton-text-box">
+        <div className="skeleton-text"></div>
+        <div className="skeleton-text short"></div>
+      </div>
       <div className="skeleton-image"></div>
-      <div className="skeleton-text"></div>
-      <div className="skeleton-text short"></div>
     </div>
   );
 };
